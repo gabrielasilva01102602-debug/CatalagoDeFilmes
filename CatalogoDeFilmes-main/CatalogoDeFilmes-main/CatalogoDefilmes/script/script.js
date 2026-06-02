8f7b680d111d5a465395240a02e62ec98788bd2e
gabrielasilva01102602-debug <gabriela.silva01102602@sesisenaipr.org.br>
2026-06-02 09:16:11 -0300

    .

--- CatalogoDeFilmes-main/CatalogoDeFilmes-main/CatalogoDefilmes/script/script.js
+++ CatalogoDeFilmes-main/CatalogoDeFilmes-main/CatalogoDefilmes/script/script.js
@@ -1,56 +1,51 @@
 const filmes = [
     {
-        titulo: "Iron Man",
-        Imagem: "https://media.themoviedb.org/t/p/w300_and_h450_face/mqN7RxojEiPoh3FTSTwOtwg7KAu.jpg",
-        descricao: "Homem de ferro"
+        titulo: "Vingadores",
+        imagem: "https://media.themoviedb.org/t/p/w600_and_h900_face/9fRX8UKlIW7Lb9GqNsJVakWWFCi.jpg",
+        descricao: "Ultimato",
     },
     {
         titulo: "Batman",
-        Imagem: "https://media.themoviedb.org/t/p/w300_and_h450_face/4lj1ikfsSmMZNyfdi8R8Tv5tsgb.jpg",
-        descricao: "O Cavaleiro das Trevas"
+        imagem: "https://media.themoviedb.org/t/p/w600_and_h900_face/4lj1ikfsSmMZNyfdi8R8Tv5tsgb.jpg",
+        descricao: "Cavaleiro das trevas",
     },
     {
-        titulo: "Pantera Negra",
-        Imagem: "https://media.themoviedb.org/t/p/w300_and_h450_face/ubXNpxL2ASSzY0f8Hxv08pOsV2L.jpg",
-        descricao: "Black Panther"
+        titulo: "Homem-Aranha",
+        imagem: "https://media.themoviedb.org/t/p/w600_and_h900_face/xaKydnMw6wR1MBAjS5seGPVusbs.jpg",
+        descricao: "Sem volta para casa",
     },
     {
-        titulo: "Velozes e Furiosos",
-        Imagem: "https://media.themoviedb.org/t/p/w300_and_h450_face/cmhFOjy47UqrOafl0oQiygcJ7oT.jpg",
-        descricao: "Turbo-Charged Prelude"
+        titulo: "As Branquelas",
+        imagem: "https://media.themoviedb.org/t/p/w600_and_h900_face/aJZOcorpgloDLkPP6ED0t9sXjNu.jpg",
+        descricao: "Dois agentes do FBI atrapalhados",
     },
     {
-        titulo: "Velozes e Furiosos",
-        Imagem: "https://media.themoviedb.org/t/p/w300_and_h450_face/1kzW2GImY1YVmLRx3NLhXFBfLLO.jpg",
-        descricao: "Desafio em Tóquio"
+        titulo: "Projeto Almanaque",
+        imagem: "https://media.themoviedb.org/t/p/w600_and_h900_face/2krLUKMsKbnZk6j647IlkqlMZ2u.jpg",
+        descricao: "Jovens descobem uma máquina do tempo",
     },
     {
-        titulo: "Tá dando Onda",
-        Imagem: "https://media.themoviedb.org/t/p/w300_and_h450_face/edR3BjwAwUGHtOQMXJTWRfMYBvt.jpg",
-        descricao: "Estréia de João Carlos"
-    },
-    {
-        titulo: "Deadpool",
-        Imagem:"https://media.themoviedb.org/t/p/w300_and_h450_face/tekExNqal2kcHp8BKiHX5jjxK9J.jpg",
-        descricao:"O anti herói"
+        titulo: "Mortal Kombat",
+        imagem: "https://media.themoviedb.org/t/p/w600_and_h900_face/4nW5X9iBrMlHMDcTsEOQWXKu3TZ.jpg",
+        descricao: "Lutando contra inimigos da Exoterra",
     }
 ]
-const listaFilmes = document.getElementById
-("listaFilmes");
-function mostrarFilmes(lista){
+
+const listaFilmes = document.getElementById("listaFilmes");
+function mostrarFilmes(lista) {
     listaFilmes.innerHTML = ""
     lista.forEach(filme => {
         listaFilmes.innerHTML += `
             <div class="col-md-4">
                 <div class="card card-filme h-100">
                     <img src="${filme.imagem}" class="card-img-top">
-                        <div class="card-body">
-                         <h3>${filme.titulo}</h3>
-                         <p>${filme.descricao}</p>
-                         <button class="btn btn-dark" onclick="verDetalhes('${filme.titulo}')">Ver detalhes</button>
+                    <div class="card-body">
+                        <h3>${filme.titulo}</h3>
+                        <p>${filme.descricao}</p>
+                        <button class="btn btn-dark" onclick="verDetalhes('${filme.titulo}')">Ver detalhes"</button>
                     </div>
                 </div>
-            </div> 
+            </div>    
         `
     });
 }
