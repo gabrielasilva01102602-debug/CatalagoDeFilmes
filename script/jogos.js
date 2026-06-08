c7cc185b74503a0459777e3c86d11c3a4769d9fa
gabrielasilva01102602-debug <gabriela.silva01102602@sesisenaipr.org.br>
2026-06-08 11:24:58 -0300

    .

--- script/jogos.js
+++ script/jogos.js
@@ -1,33 +1,31 @@
 const jogos = [
     {
-        titulo: "The Chosen",
-        imagem: "https://media.themoviedb.org/t/p/w300_and_h450_face/9Um6qPWSWHP8cl60fTtOU15uXqb.jpg",
-        descricao: "Aprofundando-se nas histórias de fundo e no contexto das pessoas e eventos dos evangelhos, a Primeira Temporada do projeto de mídia de maior financiamento coletivo pelo público de todos os tempos apresenta pessoas como Simão Pedro, Nicodemos, Maria Madalena, Mateus e, claro, Jesus de uma forma nunca antes vista em filme.",
-    },
+        titulo: "Jurassic World Evolution - Ps4",
+        imagem: "https://cdn.awsli.com.br/600x1000/53/53761/produto/29858445/eaf286a296.jpg",
+        descricao: ""
     {
-        titulo: "Os Simpsons",
+        titulo: "Pragmata - PS5",
         imagem: "https://media.themoviedb.org/t/p/w300_and_h450_face/1jGPgDDcmg3Xgs3WO8sfYfbnFoA.jpg",
-        descricao: "Uma animação sobre uma típica família dos Estados Unidos. Homer é o pai de família nada saudável ou inteligente, que adora beber cerveja. Marge é a esposa e mãe de família dedicada. Bart é o filho de 10 anos, que não leva a escola a sério e tem orgulho disso. Lisa é a garota de 8 anos, um gênio não apreciado. E Maggie é a bebê que não larga a chupeta",
+        descricao: "",
     },
     {
-        titulo: "O Show de Tom e Jerry",
-        imagem: "https://media.themoviedb.org/t/p/w300_and_h450_face/t0VTMU4UI8XlaryymvNKjnD1TXc.jpg",
-        descricao: "Tom, um gato cinzento atrapalhado, está sempre perseguindo Jerry, um pequeno e esperto ratinho marrom. Juntos, os dois aprontam e vivem as mais hilariantes aventuras, ao lado de Spike e Tyke (dois cachorros), Butch (um gato preto), Tuffy (um ratinho cinza-claro), Quacker (um simpático patinho amarelo) e muitos outros.",
+        titulo: "Split Fiction - PS5",
+        imagem: "https://cdn.awsli.com.br/600x450/2835/2835417/produto/354716028/spplit-595wuxxfxo.png",
+        descricao: "",
     },
     {
-        titulo: "Os Flintstones",
-        imagem: "https://media.themoviedb.org/t/p/w300_and_h450_face/kJpL6zkMyoN0CuoPKftWxEJ6Y9T.jpg",
-        descricao: "Os Flintstones (em inglês: The Flintstones) é uma série de televisão animada que foi exibida de 1960 a 1966 e criada por William Hanna e Joseph Barbera. O desenho retrata o cotidiano de uma família de classe média da Idade da Pedra. Calcula-se que já foi assistido por 300 milhões de espectadores em 80 países, sendo dublado em 22 idiomas. A série estreou às 20:30 do dia 30 de setembro de 1960, no canal ABC, e durou até 1 de abril de 1966. Entre 1 de outubro de 1992 e 1 de janeiro de 2004 foi emitida no Cartoon Network dos Estados Unidos. Os Flintstones parecem uma família típica de classe média da década de 60, mas suas aventuras são ambientadas na Idade da Pedra. Eles utilizam eletrodomésticos que são uma mistura de tecnologia com artefatos de pedra lascada e dinossauros.",
+        titulo: "Deathloop - PS5",
+        imagem: "https://m.media-amazon.com/images/I/71p+JoV6rCL._AC_UF1000,1000_QL80_.jpg",
+        descricao: "",
+    {
+        titulo: "Lego Ninjago O Filme: Video Game - Xbox One",
+        imagem: "https://m.media-amazon.com/images/I/81o-hUhRCqL.jpg",
+        descricao: "",
     },
     {
-        titulo: "iCarly",
-        imagem: "https://media.themoviedb.org/t/p/w300_and_h450_face/vX5q7GqGWJOgR2r0kAyWAtGWfD0.jpg",
-        descricao: "Carly Shay tem sua vida radicalmente mudada quando seu programa na internet torna-se um sucesso entre os jovens. Como seus pais estão viajando, Carly precisa contar com a ajuda de seu irmão e amigos para administrar sua recente fama.",
-    },
-    {
-        titulo: "Os Jovens Titãs em Ação",
-        imagem: "https://media.themoviedb.org/t/p/w300_and_h450_face/7YAFoSGn6gBhZFzuWhHaGa3jJOi.jpg",
-        descricao: "Robin, Estelar, Ravena, Mutano e Ciborgue são super-heróis que moram juntos. Quando não estão ocupados brigando e competindo, garantem a segurança de Jump City.",
+        titulo: "LEGO The Incredibles - Xbox One",
+        imagem: "https://m.media-amazon.com/images/I/81Lw7juE+UL.jpg",
+        descricao: "LEGO novamente surpreende os fãs da franquia de sucesso da Warner Bros, desta vez reunindo o mundo divertido de LEGO com o mundo magico da Disney Pixar. Em LEGO DisneyPixar Os Incríveis você terá que ajudar a nossa família favorita de super-heróis. Ajude a família Pera a derrotar todos os inimigos e salvar a cidade de Metroville dos terríveis vilões que aparecerão por lá. A família é formada por Beto e Helena Pera, que se casaram e tiveram três filhos que também possuem superpoderes, Violeta, Flecha e o bebê Zezé. Cada um deles possui um poder único, Beto possui super força, Helena possui elasticidade, Flecha possui super velocidade, Violeta pode ficar invisível e criar um campo de força para protegê-los e o Zezé é um “curinga” ele possui diversos poderes, como: metamorfose, levitação, autocombustão, etc. O game suporta até 2 jogadores. Totalmente em Português.",
     }
 ]
 
