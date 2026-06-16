1e1d455cde58e648cbe25fee48b5a11cc4c108a7
gabrielasilva01102602-debug <gabriela.silva01102602@sesisenaipr.org.br>
2026-06-16 11:56:09 -0300

    Add files via upload

---
+++ CatalagoDeFilmes-main/script/jogos.js
@@ -0,0 +1,61 @@
+const jogos = [
+    {
+        titulo: "Jurassic World Evolution - Ps4",
+        imagem: "https://cdn.awsli.com.br/600x1000/53/53761/produto/29858445/eaf286a296.jpg",
+        descricao: "Use bioengenharia para criar mais de 40 espécies de dinossauros que pensam e reagem, gerencie instalações, e enfrente desafios de segurança, espionagem corporativa e desastres naturais.",
+    },
+    {
+        titulo: "Pragmata - PS5",
+        imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT9eKWLnKVMyZtVTBTHrteUkLvL97t7-pOksQ&s",
+        descricao: "A humanidade descobriu na Lua um minério chamado Lunan, que deu origem ao material 'Luna Filament', capaz de replicar qualquer objeto. Quando as comunicações com a estação lunar são cortadas, uma equipe é enviada, mas um terremoto lunar separa o astronauta Hugh William de seus companheiros. Ferido, ele é salvo por Diana, uma androide única criada com essa tecnologia. Juntos, eles devem enfrentar robôs hostis controlados pela IA Idos para escapar.",
+    },
+    {
+        titulo: "Split Fiction - PS5",
+        imagem: "https://cdn.awsli.com.br/600x450/2835/2835417/produto/354716028/spplit-595wuxxfxo.png",
+        descricao: "A trama acompanha Mio e Zoe, duas escritoras de gêneros totalmente opostos (ficção científica e fantasia, respectivamente). Após serem conectadas a uma máquina projetada para roubar ideias criativas, elas ficam presas dentro de suas próprias imaginações. Ambas devem superar a desavença inicial e colaborar para escapar, utilizando habilidades complementares para sobreviver a cenários que vão desde dragões e trolls até ninjas cibernéticos.",
+    },
+    {
+        titulo: "Deathloop - PS5",
+        imagem: "https://m.media-amazon.com/images/I/71p+JoV6rCL._AC_UF1000,1000_QL80_.jpg",
+        descricao: " jogo de tiro em primeira pessoa onde você assume o papel de Colt, um assassino preso em um ciclo temporal na ilha de Blackreef Ciclo da morte.",
+    },
+    {
+        titulo: "Lego Ninjago O Filme: Video Game - Xbox One",
+        imagem: "https://m.media-amazon.com/images/I/81o-hUhRCqL.jpg",
+        descricao: "jogo de ação e aventura onde você defende a ilha de Ninjago do maligno Lorde Garmadon e seu Exército de Tubarões.",
+    },
+    {
+        titulo: "LEGO The Incredibles - Xbox One",
+        imagem: "https://m.media-amazon.com/images/I/81Lw7juE+UL.jpg",
+        descricao: "LEGO novamente surpreende os fãs da franquia de sucesso da Warner Bros, desta vez reunindo o mundo divertido de LEGO com o mundo magico da Disney Pixar. Em LEGO DisneyPixar Os Incríveis você terá que ajudar a nossa família favorita de super-heróis. Ajude a família Pera a derrotar todos os inimigos e salvar a cidade de Metroville dos terríveis vilões que aparecerão por lá. A família é formada por Beto e Helena Pera, que se casaram e tiveram três filhos que também possuem superpoderes, Violeta, Flecha e o bebê Zezé. Cada um deles possui um poder único, Beto possui super força, Helena possui elasticidade, Flecha possui super velocidade, Violeta pode ficar invisível e criar um campo de força para protegê-los e o Zezé é um “curinga” ele possui diversos poderes, como: metamorfose, levitação, autocombustão, etc. O game suporta até 2 jogadores. Totalmente em Português.",
+    }
+]
+
+const ListaJogos = document.getElementById("ListaJogos");
+function mostrarjogos(lista) {
+    ListaJogos.innerHTML = ""
+    lista.forEach(jogo => {
+        ListaJogos.innerHTML += `
+            <div class="col-md-4">
+                <div class="card card-filme h-100">
+                    <img src="${jogo.imagem}" class="card-img-top">
+                    <div class="card-body">
+                        <h3>${jogo.titulo}</h3>
+                        <p>${jogo.descricao}</p>
+                        <button class="btn btn-dark" onclick="verDetalhes('${jogo.titulo}, ${jogo.descricao}')">Ver detalhes</button>
+                    </div>
+                </div>
+            </div>    
+        `
+    });
+}
+
+mostrarjogos(jogos)
+
+function verDetalhes(titulo, descricao){
+    Swal.fire({
+        title: titulo,
+        text: descricao,
+        icon: "info"
+    })
+}
---
+++ CatalagoDeFilmes-main/script/script.js
@@ -0,0 +1,61 @@
+const filmes = [
+    {
+        titulo: "Vingadores",
+        imagem: "https://media.themoviedb.org/t/p/w600_and_h900_face/9fRX8UKlIW7Lb9GqNsJVakWWFCi.jpg",
+        descricao: "Após os eventos devastadores de 'Vingadores: Guerra Infinita', o universo está em ruínas devido aos esforços do Titã Louco, Thanos. Com a ajuda de aliados remanescentes, os Vingadores devem se reunir mais uma vez a fim de desfazer as ações de Thanos e restaurar a ordem no universo de uma vez por todas, não importando as consequências.",
+    },
+    {
+        titulo: "Batman",
+        imagem: "https://media.themoviedb.org/t/p/w600_and_h900_face/4lj1ikfsSmMZNyfdi8R8Tv5tsgb.jpg",
+        descricao: "Após dois anos desde o surgimento do Batman, os criminosos de Gotham City têm muito o que temer. Com a ajuda do tenente James Gordon e do promotor público Harvey Dent, Batman luta contra o crime organizado. Acuados com o combate, os chefes do crime aceitam a proposta feita pelo Coringa e o contratam para combater o Homem-Morcego.",
+    },
+    {
+        titulo: "Homem-Aranha",
+        imagem: "https://media.themoviedb.org/t/p/w600_and_h900_face/xaKydnMw6wR1MBAjS5seGPVusbs.jpg",
+        descricao: "Peter Parker é desmascarado e não consegue mais separar sua vida normal dos grandes riscos de ser um super-herói. Quando ele pede ajuda ao Doutor Estranho, os riscos se tornam ainda mais perigosos, e o forçam a descobrir o que realmente significa ser o Homem-Aranha...",
+    },
+    {
+        titulo: "As Branquelas",
+        imagem: "https://media.themoviedb.org/t/p/w600_and_h900_face/aJZOcorpgloDLkPP6ED0t9sXjNu.jpg",
+        descricao: "O que acontece quando dois atrapalhados agentes do FBI se disfarçam de princesas mega-ricas para se infiltrarem na alta sociedade? O resultado é situações extremamente engraçadas e diversão total ao ver estes dois irmãos azarados transformando-se em duas senhoritas refinadas e com estilo!",
+    },
+    {
+        titulo: "Projeto Almanaque",
+        imagem: "https://media.themoviedb.org/t/p/w600_and_h900_face/2krLUKMsKbnZk6j647IlkqlMZ2u.jpg",
+        descricao: "Um grupo de jovens acaba descobrindo instruções secretas para montar uma máquina do tempo, e decidem construir o aparelho. No entanto, os efeitos são muito diferentes do que esperavam.",
+    },
+    {
+        titulo: "Mortal Kombat",
+        imagem: "https://media.themoviedb.org/t/p/w600_and_h900_face/4nW5X9iBrMlHMDcTsEOQWXKu3TZ.jpg",
+        descricao: "O lutador de MMA Cole Young deve treinar para liberar seu verdadeiro poder para unir-se aos maiores campeões mundiais contra inimigos da Exoterra em uma batalha decisiva pelo universo.",
+    }
+]
+
+const ListaFilmes = document.getElementById("ListaFilmes");
+function mostrarFilmes(lista) {
+    ListaFilmes.innerHTML = ""
+    lista.forEach(filme => {
+        ListaFilmes.innerHTML += `
+            <div class="col-md-4">
+                <div class="card card-filme h-100">
+                    <img src="${filme.imagem}" class="card-img-top">
+                    <div class="card-body">
+                        <h3>${filme.titulo}</h3>
+                        <p>${filme.descricao}</p>
+                        <button class="btn btn-dark" onclick="verDetalhes('${filme.titulo}, ${filme.descricao}')">Ver detalhes</button>
+                    </div>
+                </div>
+            </div>    
+        `
+    });
+}
+
+mostrarFilmes(filmes)
+
+function verDetalhes(titulo, descricao){
+    Swal.fire({
+        title: titulo,
+        text: descricao,
+        icon: "info"
+    })
+}
